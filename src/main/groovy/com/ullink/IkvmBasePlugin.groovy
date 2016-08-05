3442aa337b0ff25672f1439a0faed9e584a16e19
Francois Valdy <francois.valdy@gmail.com>
2016-08-05 16:57:07 +0200

    Revert "Support for beta repository, picked from #38"
    This reverts commit 1d1978a767b82d51013a7af1f4804f1895a38650.
    
    Reason is that www.frijters.net is unreliable and will fail the builds for no reason
    Users can still specify the beta URL by setting ikvmHome = 'http://....' on the task

--- src/main/groovy/com/ullink/IkvmBasePlugin.groovy
+++ src/main/groovy/com/ullink/IkvmBasePlugin.groovy
@@ -3,7 +3,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class IkvmBasePlugin implements Plugin<Project> {
-
     void apply(Project project) {
         project.tasks.withType(Ikvm).whenTaskAdded { Ikvm task ->
             task.conventionMapping.map "ikvmVersion", { '7.2.4630.5' }
@@ -11,21 +10,9 @@ class IkvmBasePlugin implements Plugin<Project> {
                 if (System.getenv()['IKVM_HOME']) {
                     return System.getenv()['IKVM_HOME']
                 }
-                return tryToFindUrl(task.getIkvmVersion())
+                def version = task.getIkvmVersion()
+                return "http://downloads.sourceforge.net/project/ikvm/ikvm/${version}/ikvmbin-${version}.zip"
             }
-        }
-    }
-
-    String tryToFindUrl(String version){
-        String defaultUrl = "http://downloads.sourceforge.net/project/ikvm/ikvm/${version}/ikvmbin-${version}.zip"
-        String betaUrl = "http://www.frijters.net/ikvmbin-${version}.zip"
-
-        HttpURLConnection connection = new URL(defaultUrl).openConnection()
-        connection.setRequestMethod("HEAD")
-        if (connection.responseCode == 200) {
-            return defaultUrl
-        } else {
-            return betaUrl
         }
     }
 }
